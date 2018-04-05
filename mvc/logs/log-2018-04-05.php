7da0c07d1a62416480843738bdd04475a2f4d5e2
Elchin Zadeh <elchinzadeh@hotmail.com>
2018-04-05 10:52:30 +0400

    third

---
+++ mvc/logs/log-2018-04-05.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-04-05 10:39:19 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ D:\wamp64\www\school\main\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2018-04-05 10:39:19 --> Unable to connect to the database
+ERROR - 2018-04-05 10:39:27 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ D:\wamp64\www\school\main\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2018-04-05 10:39:27 --> Unable to connect to the database
